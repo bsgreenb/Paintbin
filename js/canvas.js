8332df9270811e24116bb8b1d83339f50142c845
Ben Greenberg <bsgreenb@gmail.com>
2012-02-28 21:18:08 -0800

    Committing.. I have to figure out how to give the colorPicker its own space

--- js/canvas.js
+++ js/canvas.js
@@ -51,6 +51,22 @@ $(document).ready(function() {
     $('#canvas').mouseleave(function(e){
         paint = false;
     });
+    
+    //Color picker..
+    $('#colorSelector').ColorPicker({
+        color: '#0000ff',
+        onShow: function (colpkr) {
+            $(colpkr).fadeIn(500);
+            return false;
+        },
+        onHide: function (colpkr) {
+            $(colpkr).fadeOut(500);
+            return false;
+        },
+        onChange: function (hsb, hex, rgb) {
+            $('#colorSelector div').css('backgroundColor', '#' + hex);
+        }
+    });
 
     $('#clear_canvas').click(function(){
         clickX = Array();
